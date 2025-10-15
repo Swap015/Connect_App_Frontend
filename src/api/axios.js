d6dbc167c7f4775066d7177a4a1b5a6caf2a6b85
Swapnil Motghare <swapnilmotghare44@gmail.com>
2025-10-15 11:50:04 +0530

    Modified axios.js file

--- src/api/axios.js
+++ src/api/axios.js
@@ -8,34 +8,34 @@ const api = axios.create({
 });
 
 
-// api.interceptors.response.use(
-//     (response) => response,
-//     async (error) => {
-//         const originalRequest = error.config;
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
 
-//         const refreshTokenExists = document.cookie.includes("refreshToken");
+        const refreshTokenExists = document.cookie.includes("refreshToken");
 
-//         if (
-//             error.response?.status === 401 &&
-//             !originalRequest._retry &&
-//             !originalRequest.url.endsWith("/user/login") &&
-//             !originalRequest.url.endsWith("/user/register") &&
-//             refreshTokenExists 
-//         ) {
-//             originalRequest._retry = true;
+        if (
+            error.response?.status === 401 &&
+            !originalRequest._retry &&
+            !originalRequest.url.endsWith("/user/login") &&
+            !originalRequest.url.endsWith("/user/register") &&
+            refreshTokenExists 
+        ) {
+            originalRequest._retry = true;
 
-//             try {
-//                 await axios.post(`${import.meta.env.VITE_API_URL}/user/refresh`, {}, { withCredentials: true });
-//                 return api(originalRequest); 
-//             } catch  {
+            try {
+                await axios.post(`${import.meta.env.VITE_API_URL}/user/refresh`, {}, { withCredentials: true });
+                return api(originalRequest); 
+            } catch  {
 
-//                 window.location.href = "/login";
-//             }
-//         }
+                window.location.href = "/login";
+            }
+        }
 
-//         return Promise.reject(error);
-//     }
-// );
+        return Promise.reject(error);
+    }
+);
 
 
 
